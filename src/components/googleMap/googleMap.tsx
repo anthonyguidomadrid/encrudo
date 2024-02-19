fd833667068507b889c0c598ce2534d07add7c61
Anthony Guido <anthony.guido@mifarma.es>
2024-02-19 20:47:10 +0100

    replace google maps url

--- src/components/googleMap/googleMap.tsx
+++ src/components/googleMap/googleMap.tsx
@@ -1,6 +1,6 @@
 export const GoogleMap = () => (
   <iframe
-    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3035.8347801655286!2d-3.675524188878447!3d40.456793653081554!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47987f519e574fdd%3A0xd24e1843506f629a!2sEncrudo%20Taller!5e0!3m2!1sen!2ses!4v1707858811766!5m2!1sen!2ses"
+    src={process.env.NEXT_PUBLIC_GOOGLE_MAPS_URL}
     width="100%"
     height="450"
     style={{
