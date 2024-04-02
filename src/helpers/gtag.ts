1d739dcee2fae6316c894684e3b9326d3a105de9
Anthony Guido <anthony.guido@mifarma.es>
2024-04-02 22:18:22 +0200

    pass all paramas GA events

--- src/helpers/gtag.ts
+++ src/helpers/gtag.ts
@@ -3,11 +3,11 @@ export const GTAG_TRACKING_ID = process.env.NEXT_PUBLIC_GTAG_ID
 
 type CustomEvent = 'form_contact'
 type CustomParams = {
-  name: string;
-  email: string;
-  phone: string;
-  message: string;
-}
+  name?: string;
+  email?: string;
+  phone?: string;
+  message?: string;
+} 
 
 /**
  * This function is used to track page views.
@@ -24,10 +24,6 @@ export const pageView = (url: URL) => {
  * https://developers.google.com/analytics/devguides/collection/gtagjs/events
  * If you want to track events, you can use the gtag.js event method.
  * */
-export const event = (action: Gtag.EventNames | CustomEvent, { event_category, event_label, value }: Gtag.EventParams & CustomParams) => {
-  window.gtag('event', action, {
-    event_category,
-    event_label,
-    value
-  })
+export const event = (action: Gtag.EventNames | CustomEvent, params: Gtag.EventParams & CustomParams) => {
+  window.gtag('event', action, params)
 }
