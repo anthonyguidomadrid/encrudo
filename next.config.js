fd669e3e736e039da101b284aa7ef825e29fa3b5
Anthony Guido <guido.anthony@orange.fr>
2025-07-31 11:32:26 +0200

    chore: improve tree shaking

--- next.config.js
+++ next.config.js
@@ -15,46 +15,24 @@ const { withPlugins } = nextComposePlugins.extend(() => ({}))
  */
 module.exports = withPlugins(plugins, {
   i18n,
-  /**
-   * add the environment variables you would like exposed to the client here
-   * documentation: https://nextjs.org/docs/api-reference/next.config.js/environment-variables
-   */
   env: {
     ENVIRONMENT_NAME: process.env.ENVIRONMENT_NAME
   },
-
-  /**
-   * The experimental option allows you to enable future/experimental options
-   * like React 18 concurrent features.
-   */
   experimental: {
     // urlImports: true,
     // concurrentFeatures: true,
     // serverComponents: true,
   },
-
-  /**
-   * SWC minification opt-in
-   * Please note that while not in experimental, the swcMinification may cause issues in your build.
-   * example: https://github.com/vercel/next.js/issues/30429 (Yup email validation causes an exception)
-   */
-  // swcMinify: true,
-
+  swcMinify: true, // Enable SWC minification for better tree shaking
   poweredByHeader: false,
   reactStrictMode: false,
   compress: true,
-
-  /**
-   * add the headers you would like your next server to use
-   * documentation: https://nextjs.org/docs/api-reference/next.config.js/headers
-   *                https://nextjs.org/docs/advanced-features/security-headers
-   */
+  // Remove legacy polyfills and enable production tree shaking
+  modularizeImports: {},
+  // Ensure only used code is bundled
+  optimizeFonts: true,
+  optimizeCss: true,
   headers,
-
-  /**
-   * https://nextjs.org/docs/basic-features/image-optimization
-   * Settings are the defaults
-   */
   images: {
     remotePatterns: [
       {
@@ -65,15 +43,20 @@ module.exports = withPlugins(plugins, {
       }
     ]
   },
-
   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
-
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack']
     })
-
+    // Remove moment.js locales if present
+    config.plugins = config.plugins || [];
+    config.plugins.push(
+      new (require('webpack')).IgnorePlugin({
+        resourceRegExp: /^\.\/locale$/,
+        contextRegExp: /moment$/
+      })
+    );
     return config
   }
 })
