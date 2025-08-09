1b8f507450ded31512ab863b697e04fe709739e3
Anthony Guido <guido.anthony@orange.fr>
2025-08-09 19:05:27 +0200

    fix: tile design mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
- 
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
+      screens: {
+        sm: '500px'
+      },
       colors: {
         primary: '#605f5f',
         grey: {
@@ -24,8 +27,8 @@ module.exports = {
       fontFamily: {
         sans: ['Montserrat'],
         header: ['Italiana']
-      },
-    },
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
