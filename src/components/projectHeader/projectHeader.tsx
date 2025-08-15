2962f308205bd6b9651b4990ac8d4f0f98d6cbaf
Anthony Guido <guido.anthony@orange.fr>
2025-08-15 14:36:28 +0200

    feat: darker header image projects

--- src/components/projectHeader/projectHeader.tsx
+++ src/components/projectHeader/projectHeader.tsx
@@ -32,17 +32,20 @@ export const ProjectHeader = ({
     <>
       <header
         role="banner"
-        className={`h-[80vh] flex flex-col justify-center text-center text-white p-10 drop-shadow bg-gray-400 bg-blend-multiply\tbg-cover bg-no-repeat bg-center`}
+        className={`relative h-[80vh] flex flex-col justify-center text-center text-white p-10 bg-cover bg-no-repeat bg-center`}
         style={{
           backgroundImage: `url(${image?.url})`
         }}
       >
-        <h1 className="pb-3">{title}</h1>
-        <h2 className="pb-4 text-lg uppercase">{subtitle}</h2>
-        <div
-          dangerouslySetInnerHTML={{ __html: description }}
-          className="max-w-4xl mx-auto space-y-4"
-        />
+        <div className="absolute inset-0 bg-black/60 z-0" />
+        <div className="relative z-10">
+          <h1 className="pb-3">{title}</h1>
+          <h2 className="pb-4 text-lg uppercase">{subtitle}</h2>
+          <div
+            dangerouslySetInnerHTML={{ __html: description }}
+            className="max-w-4xl mx-auto space-y-4"
+          />
+        </div>
       </header>
       <Breadcrumbs items={BREADCRUMBS} />
     </>
