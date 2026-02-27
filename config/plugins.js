5c56bc26691b3d8dacf7352933104f15a41ece43
Anthony Guido <guido.anthony@orange.fr>
2026-02-27 11:13:12 +0100

    chore: add runtime caching

--- config/plugins.js
+++ config/plugins.js
@@ -1,6 +1,53 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')
 const withPWA = require('next-pwa')
 
+const runtimeCaching = [
+  {
+    urlPattern: /\/\/_next\/static\/.*$/i,
+    handler: 'CacheFirst',
+    options: {
+      cacheName: 'next-static',
+      expiration: {
+        maxEntries: 256,
+        maxAgeSeconds: 60 * 60 * 24 * 365
+      }
+    }
+  },
+  {
+    urlPattern: /\/\/_next\/image\?.*$/i,
+    handler: 'StaleWhileRevalidate',
+    options: {
+      cacheName: 'next-image',
+      expiration: {
+        maxEntries: 256,
+        maxAgeSeconds: 60 * 60 * 24 * 30
+      }
+    }
+  },
+  {
+    urlPattern: /^https?:.*\.(?:woff2?|ttf|otf|eot)$/i,
+    handler: 'CacheFirst',
+    options: {
+      cacheName: 'font-assets',
+      expiration: {
+        maxEntries: 64,
+        maxAgeSeconds: 60 * 60 * 24 * 365
+      }
+    }
+  },
+  {
+    urlPattern: /^https?:.*\.(?:png|gif|jpg|jpeg|webp|svg|ico)$/i,
+    handler: 'CacheFirst',
+    options: {
+      cacheName: 'image-assets',
+      expiration: {
+        maxEntries: 256,
+        maxAgeSeconds: 60 * 60 * 24 * 30
+      }
+    }
+  }
+]
+
 module.exports = [
   [
     withPWA,
@@ -9,7 +56,9 @@ module.exports = [
         disable: process.env.NODE_ENV !== 'production',
         dest: `public`,
         register: false,
-        swSrc: './service-worker.js',
+        runtimeCaching,
+        skipWaiting: true,
+        clientsClaim: true,
         publicExcludes: ['!favicon/**/*']
       }
     }
